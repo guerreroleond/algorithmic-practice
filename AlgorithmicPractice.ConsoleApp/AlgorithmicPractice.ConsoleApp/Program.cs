97ecd98bd3cefe175e6d43f1aed7270dab13f310
David Guerrero <guerreroleond@gmail.com>
2026-04-04 17:12:59 -0600

    Implement Two Sum II solution and update Program.cs to include example usage

--- AlgorithmicPractice.ConsoleApp/AlgorithmicPractice.ConsoleApp/Program.cs
+++ AlgorithmicPractice.ConsoleApp/AlgorithmicPractice.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AlgorithmicPractice.ConsoleApp.Problems.SlidingWindow;
+using AlgorithmicPractice.ConsoleApp.Problems.TwoPointers;
 
 Console.WriteLine("Algorithmic Practice");
 
@@ -14,19 +15,30 @@ Console.WriteLine("Algorithmic Practice");
 //
 // #endregion Sliding Window - Longest Substring Without Repeating
 
-#region Sliding Window - Longest Repeating Character Replacement
+// #region Sliding Window - Longest Repeating Character Replacement
+//
+// var input = "AABBBCACCCD";
+// var k = 2;
+//
+// Console.WriteLine(
+//     $"Debug placeholder for {nameof(LongestRepeatingCharacterReplacement)}" +
+//                     $"with input '{input}' and k={k}.");
+//
+// var result = LongestRepeatingCharacterReplacement
+//                 .SolveStringCleanedUp(input, k);
+//
+// Console.WriteLine($" Longest repeating substring {result}");
+// Console.ReadLine();
+//
+// #endregion Sliding Window - Longest Repeating Character Replacement
 
-var input = "AABBBCACCCD";
-var k = 2;
+#region Two Pointers - Two Sum II
 
-Console.WriteLine(
-    $"Debug placeholder for {nameof(LongestRepeatingCharacterReplacement)}" +
-                    $"with input '{input}' and k={k}.");
+var numbers = new int[] { 2, 7, 11, 15 };
 
-var result = LongestRepeatingCharacterReplacement
-                .SolveStringCleanedUp(input, k);
+var result = TwoSumII.Solve(numbers, 26);
 
-Console.WriteLine($" Longest repeating substring {result}");
+Console.WriteLine($"Two Sum II result: [{string.Join(", ", result)}]");
 Console.ReadLine();
 
-#endregion Sliding Window - Longest Repeating Character Replacement
+#endregion Two Pointers - Two Sum II
