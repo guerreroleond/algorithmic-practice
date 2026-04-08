a89ea747d4649c8e818aea9d2b63132d562ae018
guerreroleond <guerreroleond@gmail.com>
2026-04-07 20:35:25 -0600

    Ap005 trapping rain water (#6)
    * New empty class TrappingRainWater
    
    * TrappingRainWater solution (and cleaned up version) and update Program.cs for example usage

--- AlgorithmicPractice.ConsoleApp/AlgorithmicPractice.ConsoleApp/Problems/TwoPointers/TrappingRainWater.cs
+++ AlgorithmicPractice.ConsoleApp/AlgorithmicPractice.ConsoleApp/Problems/TwoPointers/TrappingRainWater.cs
@@ -2,8 +2,69 @@ namespace AlgorithmicPractice.ConsoleApp.Problems.TwoPointers;
 
 public class TrappingRainWater
 {
+
+    public static int SolveCleanedUp(int[] peaks)
+    {
+        int left = 0;
+        int right = peaks.Length - 1;
+        int maxLeft = 0;
+        int maxRight = 0;
+        int water = 0;
+
+        while(left < right)
+        {
+            if(peaks[left] < peaks[right])
+            {
+                if(peaks[left] >= maxLeft)
+                    maxLeft = peaks[left];
+                else
+                    water += maxLeft - peaks[left];
+
+                left++;
+            }
+            else
+            {
+                if(peaks[right] >= maxRight)
+                    maxRight = peaks[right];
+                else
+                    water += maxRight - peaks[right];
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+
     public static int Solve(int[] peaks)
     {
-        return 0;
+        var left = 0;
+        var right = peaks.Length - 1;
+        var maxLeft = peaks[left];
+        var maxRight = peaks[right];
+        int current = (maxLeft < maxRight) ? left : right;
+        int water = 0;
+        
+        while(left < right)
+        {
+            var lowMax = Math.Min(maxLeft, maxRight);
+            var currWater = lowMax - peaks[current];
+            water += (currWater > 0) ? currWater : 0;
+
+            if(maxLeft < maxRight)
+            {
+                left++;
+                maxLeft = Math.Max(maxLeft, peaks[left]);
+                current = left;
+            } 
+            else
+            {
+                right--;
+                maxRight = Math.Max(maxRight, peaks[right]);
+                current = right;                 
+            } 
+        }
+
+        return water;
     }
 }
--- AlgorithmicPractice.ConsoleApp/AlgorithmicPractice.ConsoleApp/Program.cs
+++ AlgorithmicPractice.ConsoleApp/AlgorithmicPractice.ConsoleApp/Program.cs
@@ -1,11 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using AlgorithmicPractice.ConsoleApp.Problems.SlidingWindow;
 using AlgorithmicPractice.ConsoleApp.Problems.TwoPointers;
 
 Console.WriteLine("Algorithmic Practice");
 
-
-// #region Sliding Window - Longest Substring Without Repeating
+#region Sliding Window - Longest Substring Without Repeating
 //
 // var input = "VDFVABCDAJKLCB";
 //
@@ -13,9 +12,9 @@ Console.WriteLine("Algorithmic Practice");
 //
 // Console.WriteLine($" longest substring length {result}");
 //
-// #endregion Sliding Window - Longest Substring Without Repeating
+#endregion Sliding Window - Longest Substring Without Repeating
 
-// #region Sliding Window - Longest Repeating Character Replacement
+#region Sliding Window - Longest Repeating Character Replacement
 //
 // var input = "AABBBCACCCD";
 // var k = 2;
@@ -28,49 +27,36 @@ Console.WriteLine("Algorithmic Practice");
 //                 .SolveStringCleanedUp(input, k);
 //
 // Console.WriteLine($" Longest repeating substring {result}");
-// Console.ReadLine();
 //
-// #endregion Sliding Window - Longest Repeating Character Replacement
+#endregion Sliding Window - Longest Repeating Character Replacement
 
-// #region Two Pointers - Two Sum II
+#region Two Pointers - Two Sum II
 //
 // var numbers = new int[] { 2, 7, 11, 15 };
 //
 // var result = TwoSumII.Solve(numbers, 26);
 //
 // Console.WriteLine($"Two Sum II result: [{string.Join(", ", result)}]");
-// Console.ReadLine();
 //
-// #endregion Two Pointers - Two Sum II
-
-#region Two Pointers - Valid Palindrome
-
-// var numbers = new int[] { 2, 7, 11, 15 };
-
-// var result = TwoSumII.Solve(numbers, 26);
-
-// Console.WriteLine($"Two Sum II result: [{string.Join(", ", result)}]");
-
-
 #endregion Two Pointers - Two Sum II
 
-// #region Two Pointers - Trapping Rain Water
-//
-// var peaks = new int[] { 4,2,0,3,2,5 };
-//
-// var trappedWater = TrappingRainWater.Solve(peaks);
-//
-// Console.WriteLine($"Trapping Rain Water result: {trappedWater}");
-// Console.ReadLine();
-//
-// #endregion Two Pointers - Trapping Rain Water
+#region Two Pointers - Valid Palindrome
+// var input = "A man, a plan, a canal: Panama";
 
-// Console.ReadLine();
-var input = "A man, a plan, a canal: Panama";
+// var result = ValidPalindrome.Solve(input);
 
-var result = ValidPalindrome.Solve(input);
-
-Console.WriteLine($"Valid Palindrome result: {result}");
-Console.ReadLine();
+// Console.WriteLine($"Valid Palindrome result: {result}");
 
 #endregion Two Pointers - Valid Palindrome
+
+#region Two Pointers - Trapping Rain Water
+
+var peaks = new int[] { 4,2,0,3,2,5 };
+
+var trappedWater = TrappingRainWater.Solve(peaks);
+
+Console.WriteLine($"Trapping Rain Water result: {trappedWater}");
+
+#endregion Two Pointers - Trapping Rain Water
+
+Console.ReadLine();
