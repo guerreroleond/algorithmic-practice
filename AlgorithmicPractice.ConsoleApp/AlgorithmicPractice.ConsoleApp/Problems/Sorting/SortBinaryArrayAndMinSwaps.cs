aef8ea08edf5e2616ea472549954bb79d9b02e37
David Guerrero <guerreroleond@gmail.com>
2026-04-10 22:33:17 -0600

    Implement sorting of binary array with minimum swaps and update Program.cs for example usage

---
+++ AlgorithmicPractice.ConsoleApp/AlgorithmicPractice.ConsoleApp/Problems/Sorting/SortBinaryArrayAndMinSwaps.cs
@@ -0,0 +1,74 @@
+namespace AlgorithmicPractice.ConsoleApp.Problems.Sorting;
+
+
+/// <summary>
+/// Given an array of binary digits, 0 and 1, 
+/// sort the array so that all zeros are at one end and all ones are at the other. 
+/// Which end doesn’t matter. To sort the array, swap any two adjacent elements. 
+/// Determine the minimum number of swaps to sort the array.
+/// </summary>
+public class SortBinaryArrayAndMinSwaps
+{
+
+    /// <summary>
+    /// Counts the cost of moving each 0 (or 1) to its sorted position, 
+    /// by calculating and accumulating
+    /// how far each 0 (or 1) is from its target position,
+    /// then returns the minimum of both costs.
+    /// </summary>
+    /// <param name="nums">Binary array containing only 0s and 1s.</param>
+    /// <returns>Minimum number of swaps to sort the array.</returns>
+    public static int Solve(int[] nums)
+    {
+        int targetIndex0 = 0;
+        int targetIndex1 = 0;
+        int swaps0 = 0;
+        int swaps1 = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0)
+            {// How far is for its target position.
+                swaps0 += i - targetIndex0;
+                targetIndex0++;
+            }
+            if (nums[i] == 1)
+            {// How far is for its target position.
+                swaps1 += i - targetIndex1;
+                targetIndex1++;
+            }
+        }
+
+        return Math.Min(swaps0, swaps1);
+    }
+
+    /// <summary>
+    /// Counts the cost of moving each 0 (or 1) to its sorted position, 
+    /// by calculating and accumulating
+    /// how many 0s (or 1s) are blocking me on the left, 
+    /// then returns the minimum of both.
+    /// </summary>
+    /// <param name="nums">Binary array containing only 0s and 1s.</param>
+    /// <returns>Minimum number of swaps to sort the array.</returns>
+    public static int SolveAlt(int[] nums)
+    {
+        int onesSeen = 0, swaps0Left = 0;
+        int zerosSeen = 0, swaps1Left = 0;
+
+        foreach (var num in nums)
+        {
+            if (num == 1)
+            {
+                onesSeen++;
+                swaps1Left += zerosSeen;
+            }
+            else
+            {
+                zerosSeen++;
+                swaps0Left += onesSeen;
+            }
+        }
+
+        return Math.Min(swaps0Left, swaps1Left);
+    }
+}
--- AlgorithmicPractice.ConsoleApp/AlgorithmicPractice.ConsoleApp/Program.cs
+++ AlgorithmicPractice.ConsoleApp/AlgorithmicPractice.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using AlgorithmicPractice.ConsoleApp.DataStructures.LinkedList;
+using AlgorithmicPractice.ConsoleApp.Problems.Sorting;
+using AlgorithmicPractice.ConsoleApp.Problems.DataStructures.LinkedList;
 using AlgorithmicPractice.ConsoleApp.Problems.SlidingWindow;
 using AlgorithmicPractice.ConsoleApp.Problems.TwoPointers;
 
@@ -73,33 +74,26 @@ Console.WriteLine("Algorithmic Practice");
 //
 #endregion Hybrid - Singly Linked List Max Pages
 
-#region LinkedList - Remove Nth Node From End
+// #region LinkedList - Remove Nth Node From End
+//
+// var head = new SinglyLinkedListNode { PagesCount = 1, Next = new SinglyLinkedListNode { PagesCount = 2, Next = new SinglyLinkedListNode { PagesCount = 3, Next = new SinglyLinkedListNode { PagesCount = 4, Next = new SinglyLinkedListNode { PagesCount = 5 } } } } };
+//
+// var result = RemoveNthNodeFromEnd.Solve(head, 2);
+//
+// Console.WriteLine($"Remove Nth Node From End result head PagesCount: {result?.PagesCount}");
+//
+// #endregion LinkedList - Remove Nth Node From End
 
-var head = new SinglyLinkedListNode
-{
-    PagesCount = 1,
-    Next = new SinglyLinkedListNode
-    {
-        PagesCount = 2,
-        Next = new SinglyLinkedListNode
-        {
-            PagesCount = 3,
-            Next = new SinglyLinkedListNode
-            {
-                PagesCount = 4,
-                Next = new SinglyLinkedListNode
-                {
-                    PagesCount = 5
-                }
-            }
-        }
-    }
-};
+#region Sorting - Sort Binary Array And Min Swaps
 
-var result = RemoveNthNodeFromEnd.Solve(head, 2);
+var nums = new int[] { 1, 0, 0, 0, 1, 0, 0, 1 };
 
-Console.WriteLine($"Remove Nth Node From End result head PagesCount: {result?.PagesCount}");
+//var nums = new int[] { 0, 1, 0, 1};
 
-#endregion LinkedList - Remove Nth Node From End
+var result = SortBinaryArrayAndMinSwaps.SolveAlt(nums);
+
+Console.WriteLine($"Sort Binary Array Min Swaps result: {result}");
+
+#endregion Sorting - Sort Binary Array And Min Swaps
 
 Console.ReadLine();
